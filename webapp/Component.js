0f664ed4c8de1c5be326ba04f32dab5e2b9d6bd2
julioSolis1986 <julio.fsolis@gmail.com>
2018-02-15 13:21:09 -0600

    init

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("list.ordersmysalesorders.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("list.ordersmysalesorders.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
